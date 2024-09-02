1f58ef72ba4262a075ce427115d11098933977da
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-02 21:41:46 +0530

    shell

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "Ramesh : Hi Suresh, How are you?"
+echo "Suresh : Hi ramesh, I am good. How about you?"
+echo "Ramesh : I am good Suresh."
+echo "Suresh : Okay Ramesh."

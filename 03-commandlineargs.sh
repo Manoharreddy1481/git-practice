723c7f82c0bd4260609f80fc9ec63f0980ed70cf
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-02 21:44:38 +0530

    shell

---
+++ 03-commandlineargs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : Hi $PERSON2, How are you?"
+echo "$PERSON2 : Hi $PERSON1, I am good. How about you?"
+echo "$PERSON1 : I am good $PERSON2."
+echo "$PERSON2 : Okay $PERSON1."

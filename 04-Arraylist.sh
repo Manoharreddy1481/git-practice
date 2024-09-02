036a2d471535ddb60431cdef138382b07c30108a
Manoharreddy1481 <pullagurlamanoharreddy@gmail.com>
2024-09-02 21:50:03 +0530

    shell

--- 04-Arraylist.sh
+++ 04-Arraylist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-fruits=("Apple""Banana""Kiwi")
-echo "1st fruit is : $fruits[0]"
-echo "2nd fruit is : $fruits[1]"
-echo "3rd fruit is : $fruits[0]"
-echo "All fruits are : $fruits[@]"
+fruits=("Apple" "Banana" "Kiwi")
+echo "1st fruit is : ${fruits[0]}"
+echo "2nd fruit is : ${fruits[1]}"
+echo "3rd fruit is : ${fruits[0]}"
+echo "All fruits are : ${fruits[@]}"
